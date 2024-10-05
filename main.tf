3161e52db9d5d0824558295d8f321ee0cde4f701
Mahadevan45 <marellamahadevan45@gmail.com>
2024-10-05 06:44:33 +0530

    security-group

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "aws_security_group" "allow_tls" {
+resource "aws_security_group" "main" {
   name        = local.sg_final_name
   description = "Allow TLS inbound traffic and all outbound traffic"
   vpc_id      = var.vpc_id
@@ -11,6 +11,10 @@ resource "aws_security_group" "allow_tls" {
   }
 
   tags = merge(
-
+    var.common_tags,
+    var.sg_tags,
+    {
+      Name = local.sg_final_name
+    }
   )
 }
